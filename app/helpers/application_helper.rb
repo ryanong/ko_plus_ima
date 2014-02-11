9bab381c73c76fc18edb0a76b9b065324b75dc75
Ryan Ong <ryanong@gmail.com>
2014-02-10 20:31:10 -0500

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  ACTIVITY_EMAIL = "KOplusIMAactivitiesczar@gmail.com"
+  def mail_to_activities(subject = '')
+    mail_to(ACTIVITY_EMAIL, ACTIVITY_EMAIL, subject: subject)
+  end
 end
