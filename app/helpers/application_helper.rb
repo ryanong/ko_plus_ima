2750ff314346aeb2857a98462429c4ad2ba30d31
Ryan Ong <ryanong@gmail.com>
2014-02-19 21:38:07 -0500

    all _blank links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def mail_to_activities(subject = '')
     mail_to(ACTIVITY_EMAIL, ACTIVITY_EMAIL, subject: subject)
   end
+
+  def link_to(text, address, options ={})
+    options[:target] = "_blank" if address[0..3] == "http"
+    super(text, address, options)
+  end
 end
